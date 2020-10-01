fe733d0481a714139f4ef2893d13880a4373f5e3
hwandongjoo <32835661+hwandongjoo@users.noreply.github.com>
2020-10-01 23:09:56 +0900

    포스트 서비스 추가

---
+++ src/main/java/com/myapp/web/springboot/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.myapp.web.springboot.service;
+
+import com.myapp.web.springboot.domain.posts.PostsRepository;
+import com.myapp.web.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
