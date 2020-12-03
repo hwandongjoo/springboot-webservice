baffb64c4f9c87eb297d69b37205726eaa0d899b
hwandongjoo <32835661+hwandongjoo@users.noreply.github.com>
2020-12-03 19:52:06 +0900

    5.3 구글 연동하기

---
+++ src/main/java/com/myapp/web/springboot/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.myapp.web.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email); //이메일로 가입여부 확인
+}
