f04a569c27ee2e1233ebf05cb31e938a1c734420
hwandongjoo <32835661+hwandongjoo@users.noreply.github.com>
2020-12-08 21:37:44 +0900

    1208020 구글 로그인 적용 완료

--- src/main/java/com/myapp/web/springboot/web/IndexController.java
+++ src/main/java/com/myapp/web/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.myapp.web.springboot.web;
 
+import com.myapp.web.springboot.config.auth.dto.SessionUser;
 import com.myapp.web.springboot.service.PostsService;
 import com.myapp.web.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,27 +9,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts",postsService.findAllDesc());
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        // CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser 저장하게 설정해둠
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); 
+        // 세션에 있으면 user 값을 가져옴
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        // 없으면 그냥 진행
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post",dto);
+        model.addAttribute("post", dto);
         return "posts-update";
     }
+
 }
