ac7d4174bc34b96d3b41f365696f011fb853123f
hwandongjoo <32835661+hwandongjoo@users.noreply.github.com>
2020-12-12 19:49:27 +0900

    121220 profile.sh 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,19 +3,19 @@
 # 쉬고 있는 profile 찾기: real1이 사용중 | real2 휴식
 
 function find_idle_profile() {
-    RESPONSE_CODE = $(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile) # 현재 ngnix가 보고 있는 스프링 부트 정상 작동 여부 확인
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile) # 현재 ngnix가 보고 있는 스프링 부트 정상 작동 여부 확인
     if [ ${RESPONSE_CODE} -ge 400 ] # 400보다 크면 에러
     then
-      CURRNET_PROFILE = real2
+      CURRNET_PROFILE=real2
     else
-      CURRNET_PROFILE = $(curl -s http://localhost/profile)
+      CURRNET_PROFILE=$(curl -s http://localhost/profile)
     fi
 
     if [ ${CURRNET_PROFILE} == real1 ]
     then
-      IDLE_PROFILE = real2
+      IDLE_PROFILE=real2
     else
-      IDLE_PROFILE = real1
+      IDLE_PROFILE=real1
     fi
 
     echo "${IDLE_PROFILE}"
@@ -25,7 +25,7 @@ function find_idle_profile() {
 function find_idle_port() {
   IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE}==real1]
+  if [ ${IDLE_PROFILE} == real1]
   then
     echo "8081"
   else
